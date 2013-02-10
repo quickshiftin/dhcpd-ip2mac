6095796d0c529fa4d65c09be5dafd9623fc67271
Nathan Nobbe <nathan@moxune.com>
2013-02-09 23:46:01 -0700

    Initial commit

---
+++ ip2mac.h
@@ -0,0 +1,9 @@
+#include <stdarg.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <isc-dhcp/result.h>
+#include <dhcpctl.h>
